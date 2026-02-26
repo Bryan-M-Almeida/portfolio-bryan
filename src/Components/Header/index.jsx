db9054c7d0cb0eb8709a09ff8890629ea677f68e
Bryan <bryanmacieldealmeida06@gmail.com>
2026-02-26 16:54:35 -0400

    Feat: add svg favicon and svg logo

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,8 +4,40 @@ const Header = () => (
   <>
     <header className="bg-[#0a0a0a]/80 backdrop-blur">
       <div>
-        <a href="#" className="font-display group logo">
-          <span className="text-[#d4af37] group-hover:text-[white]">B</span>ryan
+        <a href="#" className="font-display group logo flex">
+          <span className="text-[#D4AF37] group-hover:text-[white]">B</span>ryan
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            width="40"
+            height="35"
+          >
+            <path
+              d="M30,30 L15,50 L30,70"
+              fill="none"
+              className="stroke-[#D4AF37] group-hover:stroke-white transition-colors"
+              stroke-width="8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <line
+              x1="45"
+              y1="30"
+              x2="55"
+              y2="70"
+              className="stroke-[#D4AF37] group-hover:stroke-white transition-colors"
+              stroke-width="8"
+              stroke-linecap="round"
+            />
+            <path
+              d="M70,30 L85,50 L70,70"
+              fill="none"
+              className="stroke-[#D4AF37] group-hover:stroke-white transition-colors"
+              stroke-width="8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </a>
 
         <nav>
@@ -15,7 +47,7 @@ const Header = () => (
           <a href="#contato">Contato</a>
         </nav>
 
-        <a href="orcamento" className="bg-[#d4af37] text-[#0a0a0a] btn">
+        <a href="orcamento" className="bg-[#D4AF37] text-[#0a0a0a] btn">
           Orçamento
         </a>
       </div>
